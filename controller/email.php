ff5f8c17f9cc8341c76d6ec59b5e300e0aa3012c
thomas <xsv24@hotmail.com>
2017-08-29 02:01:56 +1200

    Added Functionality for sending emails via PHPMailer

--- controller/email.php
+++ controller/email.php
@@ -1,8 +1,36 @@
 <?php
-     $headers  = "MIME-Version: 1.0\r\n";
-    $headers .= "Content-type: text/html; charset=utf-8\r\n";
-    echo $_POST["email"]. " " . $_POST["subject"] . " " . $_POST["message"];
+    require './PHPMailer/PHPMailerAutoload.php';
+    $mail = new PHPMailer();
 
-    $mail_res = mail($_POST["email"], $_POST["subject"], $_POST["message"], $headers);
-    echo $mail_res;
+    $to = "xsv2477@gmail.com";
+    $from = $_POST["email"];
+    $name = $_POST["name"];
+    $sub = $_POST["subject"];
+    $mess = $_POST["message"];
+    
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPAuth = true;
+    
+    $mail->Username = "tpcv.hopto@gmail.com";
+    $mail->Password = "Hashed247";
+    
+    $mail->SMTPSecure ="tsl";
+    $mail->Port = 587;
+    $mail->Sender = $from;
+    $mail->Subject = $sub;
+    $mail->Body= $mess;
+
+    $mail->isSMTP();
+    $mail->addReplyTo($from);
+    $mail->setFrom($from, $name);
+    $mail->addAddress($to);
+    $mail->isHtml(true);
+
+    if(!$mail->send()){
+        echo "mail fail";
+        echo $mail->ErrorInfo;
+    }else{
+        echo "mail success";
+    }
+
 ?>
